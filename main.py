f145ce8d30b4ed1f5bf3e76fe0dee92281a52743
Jekyll-007 <teenofeejay@gmail.com>
2024-10-29 18:17:43 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+my_set={1,1,2,2,2,3,4,5,5,5,6,7,8,9,9}
+print(my_set)
+my_set.add(10)
+print("updated set",my_set)
+my_set2={1,2,11,12,13,14,15,11}
+my_set3=my_set.intersection(my_set2)
+print(my_set3)
